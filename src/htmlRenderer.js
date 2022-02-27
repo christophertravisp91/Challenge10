44f18b618baebb295d11d1dd6dbbbf8859bfc5b4
TomFoolery <christophertravisp91@gmail.com>
2022-02-27 10:14:31 -0500

    added more html

--- src/htmlRenderer.js
+++ src/htmlRenderer.js
@@ -1,3 +1,65 @@
+const generateTeam = (team) => {
+    console.log(team);
+    const html = [];
+
+    const generateManager  = manager => {
+        console.log(manager);
+        let managerHtml = `
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">${manager.name} <br/>
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${manager.id}</li>
+            <li class="list-group-item">Email: ${manager.email}</li>
+            <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+            </ul>
+            </div>
+        `;
+        html.push(managerHtml);
+    }
+    const generateEngineer = engineer => {
+        console.log(engineer);
+        let engineerHtml = `
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">${engineer.name} <br/>
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${engineer.id}</li>
+            <li class="list-group-item">Email: ${engineer.email}</li>
+            <li class="list-group-item">GitHub Username: ${engineer.githubUsername}</li>
+            </ul>
+            </div>
+        `;
+        html.push(engineerHtml);
+    }
+    const generateIntern = intern => {
+        console.log(intern);
+        let internHtml = `
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">${intern.name} <br/>
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${intern.id}</li>
+            <li class="list-group-item">Email: ${intern.email}</li>
+            <li class="list-group-item">Interns School: ${intern.school}</li>
+            </ul>
+            </div>
+        `;
+        html.push(managerHtml);
+    }
+
+    for (let i = 0; i<team.length; i++) {
+        if (team[i].getRole() === "Manager") {
+            generateManager(team[i]);
+        }
+        if (team[i].getRole() === "Engineer") {
+            generateEngineer(team[i]);
+        }
+        if (team[i].getRole() === "Intern") {
+            generateIntern(team[i]);
+        }
+    }
+
+    return html.join('');
+};
+
 module.exports = team => {
     return `
     <!DOCTYPE html>
@@ -9,6 +71,14 @@ module.exports = team => {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="../Style/style.css" />
         <title> Team Profile Generator</title>
+    </head>
+    <body>
+        <header>
+        <h1> My Team </h1>
+        </header>
+
+        <main> ${generateTeam(team)} </main>
+
     </body>
     </html>
     `;
